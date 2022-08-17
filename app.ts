1112f68fb76c9665ed61691f30db90c47426d99f
freddiesmith <freddie.smith@sentium-consulting.com>
2022-08-17 15:25:51 +0100

    corss

--- app.ts
+++ app.ts
@@ -14,12 +14,17 @@ const app = express();
 passportConfig(passport);
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(cors({origin:"https://memlet-frontend.vercel.app"}));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.use("/", userAPIRouter);
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -31,7 +36,7 @@ app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  console.log(err)
   // render the error page
   res.status(err.status || 500).send("error")
 });
