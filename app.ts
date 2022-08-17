34fc5f386b793e96775158075ef861bc1efcbbd9
freddiesmith <freddie.smith@sentium-consulting.com>
2022-08-17 15:31:57 +0100

    fix

--- app.ts
+++ app.ts
@@ -15,16 +15,17 @@ passportConfig(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors());
+app.all('/*', (req, res, next) => {
+  console.log("ADDING HEADER")
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
 app.use("/", userAPIRouter);
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
