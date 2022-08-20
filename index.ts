4168298ac12d00956fe2bb1c4ca73763dedc8f58
freddiesmith <freddie.smith@sentium-consulting.com>
2022-08-20 14:19:20 +0100

    clear out

--- index.ts
+++ index.ts
@@ -23,14 +23,7 @@ app.all('/*', (req, res, next) => {
 });
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use("/", userAPIRouter);
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
 
 // error handler
 app.use((err, req, res, next) => {
