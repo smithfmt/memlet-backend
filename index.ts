8ba7894da4659a84dc2de073108ab2abb635ee06
freddiesmith <freddie.smith@sentium-consulting.com>
2022-08-20 13:52:23 +0100

    app reorg

--- index.ts
+++ index.ts
@@ -1,68 +1,49 @@
-#!/usr/bin/env node
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from "cors";
+import passport from 'passport';
+import userAPIRouter from "./api/userAPI";
+import passportConfig from "./config/passport";
 
-/**
- * Module dependencies.
- */
+const app = express();
 
-import app from './app';
-import debug from 'debug';
-debug('api:server');
-import http from 'http';
+// Passport
+passportConfig(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cors());
+app.all('/*', (req, res, next) => {
+  console.log("ADDING HEADER")
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use("/", userAPIRouter);
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
 
-const normalizePort = val => {
-  const port = parseInt(val, 10);
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.log(err)
+  // render the error page
+  res.status(err.status || 500).send("error")
+});
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
+app.listen(7000, () => {
+  console.log("Running on port 7000.");
+});
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-const onError = error => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  const bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-const onListening = () => {
-  const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
-
-const port = normalizePort(process.env.PORT || '7000');
-app.set('port', port);
-
-const server = http.createServer(app);
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
+export default app;
