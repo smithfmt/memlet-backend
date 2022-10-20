1d1001f6da261c3aa0584e3a3bed0cf4df627e87
freddiesmith <freddie.smith@sentium-consulting.com>
2022-10-20 11:46:09 +0100

    add ans cap

--- controllers/answerController.ts
+++ controllers/answerController.ts
@@ -1,9 +1,11 @@
 import { compare } from "../helpers";
 import prisma from "../libs/prisma";
+import jwt from "jsonwebtoken";
 
 export const checkAnswer = async (req, res, next) => {
     const { answer, correct, wordlistItemId, langs, strict } = req.body;
     const result = compare(answer, correct, langs, strict);
+    const id = jwt.decode(req.headers.authorization.split(" ")[1]).sub as string;
     let isCorrect = false;
     if (result[1]===100) {isCorrect=true};
     try {
@@ -14,8 +16,26 @@ export const checkAnswer = async (req, res, next) => {
                 correct: isCorrect,
                 correct_percentage: result[1] as number,
                 wordlistItemId,
+                userId: parseInt(id),
             },
         });
+        const user = await prisma.user.findUnique({
+            where: {
+                id: parseInt(id),
+            },
+            include: {
+                testAnswers: true,
+            }
+        });
+        const testAnswerList = user.testAnswers.map(ans => {return {id: ans.id, created: ans.created}}).sort((a,b) => {return a.created.getTime()-b.created.getTime()});
+        if (testAnswerList.length>500) {
+            const toDelete = testAnswerList.slice(0,testAnswerList.length-500).map(obj => {return obj.id})
+            await prisma.test_answer.deleteMany({
+                where: {
+                    id: {in: toDelete}
+                },
+            });
+        };
         res.status(200).json({ success: true, msg: "Here is your result!", result });
     } catch (e) {
         res.status(401).json({ success: false, msg: "error contacting DB", error: e })
